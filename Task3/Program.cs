8bb9114269347434b0f7fae0c275ffdbf4864868
Ovella <oks.vella@yandex.ru>
2022-11-10 16:28:55 +0300

    added task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,91 @@
+﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц
+int[,] matrixA = new int[2, 2];
+int[,] matrixB = new int[2, 2];
+int[,] matrixC = new int[2, 2];
+FillArrayA(matrixA);
+FillArrayB(matrixB);
+PrintArrayA(matrixA);
+Console.WriteLine();
+PrintArrayB(matrixB);
+
+
+
+MultiplMatrix(matrixA, matrixB, matrixC);
+Console.WriteLine($"multiplication of two matrices:");
+PrintArrayC(matrixC);
+
+void MultiplMatrix(int[,] martrixA, int[,] martrixB, int[,] matrixC)
+{
+    for (int i = 0; i < matrixC.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixC.GetLength(1); j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < matrixA.GetLength(1); k++)
+            {
+                sum += matrixA[i, k] * matrixB[k, j];
+            }
+            matrixC[i, j] = sum;
+        }
+    }
+}
+
+
+void FillArrayA(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void FillArrayB(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArrayA(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void PrintArrayB(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void PrintArrayC(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
